3f5d82c9a90db8bfc15a35bd2e794307f10a8934
Lucas Georges Helal <lucasghelal@gmail.com>
2016-12-13 21:28:47 -0200

    trabalho 3 imagem

--- trab3/otsu_imagem_luminancia.py
+++ trab3/otsu_imagem_luminancia.py
@@ -14,13 +14,32 @@ imagem_rgb = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
 image_cinza = cv2.cvtColor(imagem_rgb, cv2.COLOR_BGR2GRAY)
 
 
-ret, imgT = cv2.threshold(
-    image_cinza, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+def Otsu(img):
+    hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+    hist_norm = hist.ravel() / hist.max()
+    Q = hist_norm.cumsum()
 
+    bins = np.arange(256)
 
-# TODO implementar otsu metodo
-def Otsu(imagem):
-    pass
+    fn_min = np.inf
+    thresh = -1
+
+    for i in range(1, 256):
+        p1, p2 = np.hsplit(hist_norm, [i])  # probabilities
+        q1, q2 = Q[i], Q[255] - Q[i]  # cum sum of classes
+        b1, b2 = np.hsplit(bins, [i])  # weights
+
+        # finding means and variances
+        m1, m2 = np.sum(p1 * b1) / q1, np.sum(p2 * b2) / q2
+        v1, v2 = np.sum(((b1 - m1)**2) * p1) / \
+            q1, np.sum(((b2 - m2)**2) * p2) / q2
+
+        # calculates the minimization function
+        fn = v1 * q1 + v2 * q2
+        if fn < fn_min:
+            fn_min = fn
+            thresh = i
+    return thresh
 
 
 def Threshold(img, threshold):
@@ -31,14 +50,18 @@ def Threshold(img, threshold):
     return img
 
 
+valor_otsu = Otsu(image_cinza)
+imgT = Threshold(image_cinza, valor_otsu)
+
+
 def CorrecaoDeContrasteLinear(imagem_original, imagem_otsu):
     valores = np.where(imagem_otsu == 0)
     correcao_linear = imagem_original[valores]
     minimo = np.min(correcao_linear)
     maximo = np.max(correcao_linear)
     a = 255 / (maximo - minimo)
-    b = -a*minimo
-    correcao = np.uint8(a*correcao_linear + b)
+    b = -a * minimo
+    correcao = np.uint8(a * correcao_linear + b)
     imagem_original[valores] = correcao
     return imagem_original
 
@@ -52,6 +75,7 @@ imagem_rgb[:, :, 2] = CorrecaoDeContrasteLinear(R, imgT)
 # imgTeste = Threshold(image_cinza, 104)
 
 cv2.imshow("Correção de Contraste Linear", imagem_rgb)
+cv2.imwrite("ContrasteLinear.png", imagem_rgb)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
